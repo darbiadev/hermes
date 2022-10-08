0052057bd2a4709e76d7373288ede4d12b00dd55
Bradley Reynolds <bradley.reynolds@darbia.dev>
2022-10-08 00:13:50 -0500

    oops, only debug some things
    Signed-off-by: Bradley Reynolds <bradley.reynolds@darbia.dev>

--- tests/test_app.py
+++ tests/test_app.py
@@ -9,9 +9,5 @@ runner = CliRunner()
 
 def test_app():
     result = runner.invoke(app)
-    assert result.runner == 0
     assert result.stdout_bytes == 0
     assert result.stderr_bytes == 0
-    assert result.exit_code == 0
-    assert result.exception == 0
-    assert result.exc_info == 0
